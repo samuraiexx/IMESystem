0a5eaab5c19af03cad73bc987d2c3535018e21ce
SamuraiExx <samuraiexx@gmail.com>
2018-05-25 19:56:09 -0300

    Added dumby responses

--- Web/processor.js
+++ Web/processor.js
@@ -5,20 +5,20 @@ login = function(user, pass, super_user){
 };
 
 exports.process = function(request) {
-    var super_user = "super-user" in request && request["super-user"] == "true";
+    var super_user = "super-user" in request && request["super-user"] == true;
 
     var valid = sqlConsult.login(request["user"], request["password"], super_user);
 
 
     if(!("query" in request) || !valid)
-        return "{'valid' : '" + valid + "'}";
+        return "{'valid' : " + valid + "}";
 
     var query = request["query"];
     var filter = "";
     if("filter" in request) filter = request["filter"];
 
     if(query in ["alunos", "disciplinas", "notas"] && !super_user)
-        return "{'valid' : 'false'}";
+        return "{'valid' : false}";
 
     return sqlConsult[query](filter);
 };
--- Web/sqlConsult.js
+++ Web/sqlConsult.js
@@ -9,37 +9,37 @@ class sqlConsult{
 
     alunos(filter){
         return '[' +
-            '{"AlunoId" : "15419", "nomeAluno" : "MateusC", "anoGrad" : "2019"}, ' +
-            '{"AlunoId" : "15420", "nomeAluno" : "Matheus", "anoGrad" : "2019"}, ' +
-            '{"AlunoId" : "15421", "nomeAluno" : "Matheus", "anoGrad" : "2019"}, ' +
+            '{"AlunoId" : 15419, "nomeAluno" : "MateusC", "anoGrad" : 2019}, ' +
+            '{"AlunoId" : 15420, "nomeAluno" : "Matheus", "anoGrad" : 2019}, ' +
+            '{"AlunoId" : 15421, "nomeAluno" : "Matheus", "anoGrad" : 2019}, ' +
             ']';
     }
     disciplinas(filter){
         return '[' +
-            ' {"DisciplinaId: "1", "disciplinaNome":"Algelin"}, ' +
-            ' {"DisciplinaId: "2", "disciplinaNome":"MecII"}, ' +
-            ' {"DisciplinaId: "3", "disciplinaNome":"VeganInitiation"}, ' +
+            ' {"DisciplinaId: 1, "disciplinaNome":"Algelin"}, ' +
+            ' {"DisciplinaId: 2, "disciplinaNome":"MecII"}, ' +
+            ' {"DisciplinaId: 3, "disciplinaNome":"VeganInitiation"}, ' +
             ']';
     }
     notas(filter){
         return '[' +
             '{"nomeDisciplina" : "Algelin", notasDisciplina : [' +
-                '{"nomeAluno" : "fabinho", "notaAluno" : "10", "media" : "9.9"}, ' +
-                '{"nomeAluno" : "fabao", "notaAluno" : "9.9", "media" : "9.8"}, ' +
-                '{"nomeAluno" : "suamae", "notaAluno" : "9.9", "media" : "9.8"}, ]' +
+                '{"nomeAluno" : "fabinho", "notaAluno" : 10, "media" : 9.9}, ' +
+                '{"nomeAluno" : "fabao", "notaAluno" : 9.9, "media" : 9.8}, ' +
+                '{"nomeAluno" : "suamae", "notaAluno" : 9.9, "media" : 9.8}, ]' +
             '}, ' +
             '{"nomeDisciplina" : "Algelin2", notasDisciplina : [' +
-                '{"nomeAluno" : "fabinho", "notaAluno" : "5", "media" : "9.9"}, ' +
-                '{"nomeAluno" : "fabao", "notaAluno" : "9.9", "media" : "9.8"}, ' +
-                '{"nomeAluno" : "suamae", "notaAluno" : "9.9", "media" : "9.8"}, ]' +
+                '{"nomeAluno" : "fabinho", "notaAluno" : 5, "media" : 9.9}, ' +
+                '{"nomeAluno" : "fabao", "notaAluno" : 9.9, "media" : 9.8}, ' +
+                '{"nomeAluno" : "suamae", "notaAluno" : 9.9, "media" : 9.8}, ]' +
             '}, ' +
         ']';
     }
     appNotas(filter, user){
-        return '{"a":1,"b":2,"s":"oi","obj2":{"c":4}}';
+        return '{"VC" : 10, "VE" : 5, "VF" : 4}';
     }
     appFaltas(filter, user){
-        return '{"a":1,"b":2,"s":"oi","obj2":{"c":4}}';
+        return '{"pontos" : 119}';
     }
 }
 
