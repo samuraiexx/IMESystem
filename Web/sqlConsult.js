6783f3a94403c9a3871c1a0e3731278069780a24
Luciano Barreira <luciano@roboime.com.br>
2018-05-26 00:48:08 -0300

    Add function for generating queries and executing them

--- Web/sqlConsult.js
+++ Web/sqlConsult.js
@@ -1,46 +1,50 @@
 var fs = require('fs');
 
-class sqlConsult{
-    login(user, password, superUser) {
-        if (user == "samuraiexx" &&
-            password == "naru10") return superUser;
-        return false;
-    }
+var db = require('./db.js');
 
-    alunos(filter){
-        return '[' +
-            '{"AlunoId" : 15419, "nomeAluno" : "MateusC", "anoGrad" : 2019}, ' +
-            '{"AlunoId" : 15420, "nomeAluno" : "Matheus", "anoGrad" : 2019}, ' +
-            '{"AlunoId" : 15421, "nomeAluno" : "Matheus", "anoGrad" : 2019}, ' +
-            ']';
-    }
-    disciplinas(filter){
-        return '[' +
-            ' {"DisciplinaId: 1, "disciplinaNome":"Algelin"}, ' +
-            ' {"DisciplinaId: 2, "disciplinaNome":"MecII"}, ' +
-            ' {"DisciplinaId: 3, "disciplinaNome":"VeganInitiation"}, ' +
-            ']';
-    }
-    notas(filter){
-        return '[' +
-            '{"nomeDisciplina" : "Algelin", notasDisciplina : [' +
-                '{"nomeAluno" : "fabinho", "notaAluno" : 10, "media" : 9.9}, ' +
-                '{"nomeAluno" : "fabao", "notaAluno" : 9.9, "media" : 9.8}, ' +
-                '{"nomeAluno" : "suamae", "notaAluno" : 9.9, "media" : 9.8}, ]' +
-            '}, ' +
-            '{"nomeDisciplina" : "Algelin2", notasDisciplina : [' +
-                '{"nomeAluno" : "fabinho", "notaAluno" : 5, "media" : 9.9}, ' +
-                '{"nomeAluno" : "fabao", "notaAluno" : 9.9, "media" : 9.8}, ' +
-                '{"nomeAluno" : "suamae", "notaAluno" : 9.9, "media" : 9.8}, ]' +
-            '}, ' +
-        ']';
-    }
-    appNotas(filter, user){
-        return '{"VC" : 10, "VE" : 5, "VF" : 4}';
-    }
-    appFaltas(filter, user){
-        return '{"pontos" : 119}';
-    }
+function queryHelper(row, filter, boolop) {
+  var s = "(";
+  for (x in filter) {
+    s += row + " = '" + filter[x] + "' " + boolop + " ";
+  }
+  return s.slice(0, -4) + ")";
+}
+
+
+class sqlConsult {
+  login(user, password, superUser) {
+    if (user == "samuraiexx" &&
+      password == "naru10") return superUser;
+    return false;
+  }
+
+  // SELECT alunoId, anoGrad from aluno WHERE
+  alunos(filter,callback, callback) {
+    const query = "SELECT alunoId,nome as nomeAluno, anoGrad from aluno;"
+    db.exec(query,callback);
+  }
+  //SELECT distinct disciplina from (SELECT aluno.alunoId, nota.disciplina, nota.periodo from aluno join nota on aluno.alunoId = nota.alunoId) t where periodo = 6;
+  disciplinas(filter,callback) {
+    const query = "SELECT distinct disciplina from (SELECT aluno.alunoId," +
+      " nota.disciplina, nota.periodo from aluno join nota on" +
+      " aluno.alunoId = nota.alunoId) t where periodo = " + filter["periodo"] + ";"
+
+    db.exec(query,callback);
+  }
+  // { id : [15416,15419], periodo : 6, disciplinas}
+  notas(filter,callback) {
+    var clause = queryHelper("alunoId",filter["id"],"OR") + " AND " + queryHelper("disciplina", filter["disciplina"],"OR") + " AND periodo = " + filter["periodo"];
+    var table =  "(select a.alunoId,disciplina,periodo, (VE+VC)*0.25+VF*0.5 as media from aluno a join nota b on a.alunoId = b.alunoId order by disciplina) t"
+    var query =  "select alunoId,disciplina,media from " +table+" where" + clause + ";";
+
+    db.exec(query,callback);
+  }
+  appNotas(filter,callback, user) {
+    return '{"VC" : 10, "VE" : 5, "VF" : 4}';
+  }
+  appFaltas(filter,callback, user) {
+    return '{"pontos" : 119}';
+  }
 }
 
 module.exports = new sqlConsult();
