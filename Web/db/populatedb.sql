4cea051401ff29e74a21001b1229335008a3d4a2
Luciano Barreira <luciano@roboime.com.br>
2018-05-25 21:50:53 -0300

    Add populate and table creation scripts

---
+++ Web/db/populatedb.sql
@@ -0,0 +1,7 @@
+insert into admin values ('samuraiexx', 'naru10');
+insert into aluno values (15416, 'lucbarr', 's3nh4d1f1c1l', 'Luciano Barreira', 2019);
+insert into aluno values (15419, 'samuraiexx', 's3nh4f4c1l', 'De Castro', 2019);
+insert into nota values (15416, 6, 'LAB PROG II', 8.8, 7.0, 10.0);
+insert into nota values (15419, 6, 'LAB PROG II', 10.0, 10.0, 10.0);
+insert into falta values (15416, 7, 30);
+insert into falta values (15419, 7, 8);
---
+++ Web/db/prepdb.sql
@@ -0,0 +1,39 @@
+CREATE TABLE `dummy`.`aluno` (
+  `alunoId` INT NOT NULL,
+   `usuario` VARCHAR(45) NULL,
+  `senha` VARCHAR(255) NULL,
+   `nome` VARCHAR(45) NULL,
+  `anoGrad` SMALLINT NULL,
+   PRIMARY KEY (`alunoId`));
+
+
+CREATE TABLE `dummy`.`nota` (
+  `alunoId` INT NULL,
+  `periodo` SMALLINT NULL,
+  `disciplina` VARCHAR(45) NULL,
+  `VE` FLOAT NULL,
+  `VC` FLOAT NULL,
+  `VF` FLOAT NULL,
+  INDEX `alunoId_idx` (`alunoId` ASC),
+  CONSTRAINT `alunoId`
+    FOREIGN KEY (`alunoId`)
+      REFERENCES `dummy`.`aluno` (`alunoId`)
+        ON DELETE NO ACTION
+          ON UPDATE NO ACTION);
+
+CREATE TABLE `dummy`.`falta` (
+  `alunoId` INT NULL,
+  `periodo` VARCHAR(45) NULL,
+  `pontos` SMALLINT NULL,
+  INDEX `alunoId_idx` (`alunoId` ASC),
+  CONSTRAINT `alunoId2`
+    FOREIGN KEY (`alunoId`)
+      REFERENCES `dummy`.`aluno` (`alunoId`)
+        ON DELETE NO ACTION
+          ON UPDATE NO ACTION);
+
+CREATE TABLE `dummy`.`admin` (
+  `usuario` VARCHAR(30) NOT NULL,
+  `senha` VARCHAR(255) NOT NULL);
+
+
