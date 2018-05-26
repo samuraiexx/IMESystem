0b9ac1b8a6013d52bfc1f6a8776c03c9f26155a5
SamuraiExx <samuraiexx@gmail.com>
2018-05-26 02:57:49 -0300

    Fix in sql query

--- Web/sqlConsult.js
+++ Web/sqlConsult.js
@@ -34,7 +34,7 @@ class sqlConsult {
     // { id : [15416,15419], periodo : 6, disciplinas}
     notas(filter,callback) {
         console.log(filter);
-        var clause = queryHelper("alunoId",filter["id"],"OR") + " AND " + queryHelper("disciplina", filter["disciplina"],"OR") + " AND periodo = " + filter["periodo"];
+        var clause = queryHelper("alunoId",filter["id"],"OR") + " AND " + queryHelper("disciplina", filter["disciplinas"],"OR") + " AND periodo = " + filter["periodo"];
         var table =  "(select a.alunoId,disciplina,periodo, (VE+VC)*0.25+VF*0.5 as media from aluno a join nota b on a.alunoId = b.alunoId order by disciplina) t"
         var query =  "select alunoId,disciplina,media from " +table+" where" + clause + ";";
 
