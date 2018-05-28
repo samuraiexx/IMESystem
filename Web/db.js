b988b4b906cad43af055c4b0be736e042805ee4f
SamuraiExx <samuraiexx@gmail.com>
2018-05-28 04:29:34 -0300

    pc

--- Web/db.js
+++ Web/db.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'naru10',
+  password: 'YOURPASSWORD',
   database: 'dummy',
 });
 
